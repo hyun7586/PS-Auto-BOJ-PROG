3876322d5c1f1cecd65185614bfdc58e8d13d276
DooHyun Kim <104716006+hyun7586@users.noreply.github.com>
2025-10-25 16:42:36 +0900

    [Silver V] Title: 방 번호, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1475. 방 번호/방 번호.cc
+++ 백준/Silver/1475. 방 번호/방 번호.cc
@@ -7,7 +7,7 @@
 using namespace std;
 
 int N;
-int max_idx = 0, max_val = 0;
+int max_val = 0;
 int cnt[10] = {0};
 
 void Solve() {
@@ -19,24 +19,14 @@ void Solve() {
     N = N / 10;
   }
 
-  max_val = cnt[0];
-
   // find the max index/value element from the cnt array
   for (int i = 0; i < 10; i++) {
-    if (cnt[i] > max_val) {
-      if (i == 6 || i == 9) continue;
+    if (i == 6 || i == 9) continue;
 
-      max_idx = i;
-      max_val = cnt[i];
-    }
+    max_val = max(max_val, cnt[i]);
   }
 
-  int temp = (cnt[6] + cnt[9]) % 2 == 1 ? (cnt[6] + cnt[9]) / 2 + 1 : (cnt[6] + cnt[9]) / 2;
-
-  if (max_val >= temp)
-    cout << max_val;
-  else
-    cout << temp;
+  cout << max(max_val, (cnt[6] + cnt[9] + 1) / 2);
 }
 
 int main() {
